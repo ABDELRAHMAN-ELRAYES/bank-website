3407071ccf23c09b907a79e1ed79bc6fb5bb99ba
Abdelrahman Elrayes <abdelrahmanelrayes2@gmail.com>
2024-07-12 19:37:32 +0300

    nav bar

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
-'use strict';
+"use strict";
+let homeContents = document.querySelectorAll(".home-content");
+let amount = 0;
+homeContents.forEach(element => {
+  element.style.transform = `translateX(${amount}%)`;
+  amount += 100;
+});
