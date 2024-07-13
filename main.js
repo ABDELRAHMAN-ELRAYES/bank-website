ba84920990c696f7ae27dc56671f130125406cae
Abdelrahman Elrayes <abdelrahmanelrayes2@gmail.com>
2024-07-13 14:36:17 +0300

    some updates in home page

--- main.js
+++ main.js
@@ -1,7 +1,44 @@
 "use strict";
-let homeContents = document.querySelectorAll(".home-content");
-let amount = 0;
-homeContents.forEach(element => {
-  element.style.transform = `translateX(${amount}%)`;
-  amount += 100;
+let slides = document.querySelectorAll(".slide");
+let dots = document.querySelector(".dots");
+
+let currentSlide = 1;
+let dotCounter = 1;
+slides.forEach(element => {
+  let dot = `<div class="dot dot${dotCounter}" data-slide-number="${dotCounter++}"></div>`;
+  dots.insertAdjacentHTML("beforeend", dot);
 });
+let goToSlide = function (curSlide) {
+  slides.forEach((element, i) => {
+    element.style.transform = `translateX(${100 * (i + 1 - curSlide)}%)`;
+  });
+};
+let activateDot = function (slideNum) {
+  let allDots = dots.children;
+  Array.from(allDots).forEach(child => {
+    child.classList.remove("active-dot");
+  });
+  document.querySelector(`.dot${slideNum}`).classList.add("active-dot");
+};
+goToSlide(currentSlide);
+activateDot(currentSlide);
+
+dots.addEventListener("click", event => {
+  if (event.target.classList.contains("dot")) {
+    let slideNum = Number(event.target.dataset.slideNumber);
+    goToSlide(slideNum);
+    activateDot(slideNum);
+  }
+});
+/* let obsCallback = function (entries) {
+  let [entry] = entries;
+  if (entry.isIntersecting) nav.classList.remove('sticky-nav');
+  else nav.classList.add('sticky-nav');
+};
+let obsOptions = {
+  root: null,
+  threshold: 0,
+  rootMargin: `-${getComputedStyle(nav).height}`,
+};
+const observer = new IntersectionObserver(obsCallback, obsOptions);
+observer.observe(header); */
